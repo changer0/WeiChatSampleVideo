948058f664d8ca52d0a2c957834b5dde42d8b9a2
张鹿鹿 <zhanglulu1007@foxmail.com>
2016-10-20 19:30:15 +0800

    完成视频录制和自定义ProgressBar

--- app/src/main/java/com/lulu/weichatsamplevideo/BothWayProgressBar.java
+++ app/src/main/java/com/lulu/weichatsamplevideo/BothWayProgressBar.java
@@ -21,11 +21,11 @@ public class BothWayProgressBar extends View {
     private Paint mRecordPaint;
     //上滑取消时的画笔
     private Paint mCancelPaint;
-
+    //是否显示
     private int mVisibility;
-
+    // 当前进度
     private int progress;
-
+    //进度条结束的监听
     private OnProgressEndListener mOnProgressEndListener;
 
 
@@ -46,18 +46,17 @@ public class BothWayProgressBar extends View {
         mCancelPaint.setColor(Color.RED);
     }
 
-
-
-
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if (mVisibility == View.VISIBLE) {
             int height = getHeight();
             int width = getWidth();
+            int mid = width / 2;
+
+
             //画出进度条
-            if (progress < width/2){
+            if (progress < mid){
                 canvas.drawRect(progress, 0, width-progress, height, isCancel ? mCancelPaint : mRecordPaint);
             } else {
                 if (mOnProgressEndListener != null) {
@@ -67,10 +66,10 @@ public class BothWayProgressBar extends View {
         } else {
             canvas.drawColor(Color.argb(0, 0, 0, 0));
         }
-
-
-
     }
+
+
+
 
     /**
      * 设置进度
--- app/src/main/java/com/lulu/weichatsamplevideo/MainActivity.java
+++ app/src/main/java/com/lulu/weichatsamplevideo/MainActivity.java
@@ -1,23 +1,24 @@
 package com.lulu.weichatsamplevideo;
 
-import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.util.List;
-
-import static android.hardware.Camera.getCameraInfo;
-import static android.hardware.Camera.getNumberOfCameras;
 
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, View.OnTouchListener, BothWayProgressBar.OnProgressEndListener {
 
@@ -26,10 +27,26 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     //预览SurfaceView
     private SurfaceView mPreview;
     private Camera mCamera;
-
-    private int mCameraId;
+    //底部"按住拍"按钮
+    private View mStartButton;
+    //进度条
     private BothWayProgressBar mProgressBar;
-    private View mPressControler;
+    //进度条线程
+    private Thread mProgressThread;
+    //录制视频
+    private MediaRecorder mMediaRecorder;
+    private SurfaceHolder mSurfaceHolder;
+    //屏幕分辨率
+    private int videoWidth, videoHeight;
+    //判断是否正在录制
+    private boolean isRecording;
+    private File mTargetFile;
+    //当前进度/时间
+    private int mProgress;
+    //录制最大时间
+    public static final int MAX_TIME = 10;
+
+    private MyHandler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +54,27 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
         initView();
-
-
     }
 
     private void initView() {
+        videoWidth = 640;
+        videoHeight = 480;
         mPreview = (SurfaceView) findViewById(R.id.main_surface_view);
-        mPreview.getHolder().addCallback(this);
-        mPressControler = findViewById (R.id.main_press_control);
-        mPressControler.setOnTouchListener(this);
+        mMediaRecorder = new MediaRecorder();
+        mSurfaceHolder = mPreview.getHolder();
+        //设置屏幕分辨率
+        mSurfaceHolder.setFixedSize(videoWidth, videoHeight);
+        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mSurfaceHolder.addCallback(this);
+        mStartButton = findViewById(R.id.main_press_control);
+        mStartButton.setOnTouchListener(this);
         //自定义双向进度条    (这个地方差点把我急疯了!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)
-        mProgressBar = (BothWayProgressBar) findViewById(R.id.main_progress_bar);
+        mProgressBar = (BothWayProgressBar) findViewById(R.
+                id.main_progress_bar);
         mProgressBar.setOnProgressEndListener(this);
+
+        mHandler = new MyHandler(this);
+
     }
 
 
@@ -57,10 +83,19 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     ///////////////////////////////////////////////////////////////////////////
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        mSurfaceHolder = holder;
+        startPreView(holder);
+    }
+
+    /**
+     * 开启预览
+     * @param holder
+     */
+    private void startPreView(SurfaceHolder holder) {
         if (mCamera == null) {
-            initCamera();
+            mCamera = Camera.open();
             if (mCamera != null) {
-                setCameraDisplayOrientation(this, mCameraId, mCamera);
+                mCamera.setDisplayOrientation(90);
                 try {
                     mCamera.setPreviewDisplay(holder);
                     Camera.Parameters parameters = mCamera.getParameters();
@@ -72,86 +107,33 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
                             parameters.setFocusMode("continuous-video");
                         }
                     }
-                    mCamera.setParameters(parameters );
+                    mCamera.setParameters(parameters);
                     mCamera.startPreview();
-
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             }
         }
-
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
     }
-
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         if (mCamera != null) {
             //停止预览并释放摄像头资源
             mCamera.stopPreview();
             mCamera.release();
+
+        }
+        if (mMediaRecorder != null) {
+            mMediaRecorder.release();
         }
     }
 
 
-    /**
-     * 初始化相机
-     */
-    private void initCamera() {
-        int numberOfCameras = getNumberOfCameras();
-        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
-        for (int i = 0; i < numberOfCameras; i++) {
-            getCameraInfo(i, cameraInfo);
-            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
-                mCamera = Camera.open(i);
-                mCameraId = i;
-                break;
-            }
-        }
-    }
-
-    /**
-     * 根据手机屏幕方向, 来检测和设置摄像头预览的方法
-     *  Copy 的API文档中的代码
-     * @
-     */
-    public static void setCameraDisplayOrientation(Activity activity,
-                                                   int cameraId, android.hardware.Camera camera) {
-        android.hardware.Camera.CameraInfo info =
-                new android.hardware.Camera.CameraInfo();
-        getCameraInfo(cameraId, info);
-        int rotation = activity.getWindowManager().getDefaultDisplay()
-                .getRotation();
-        int degrees = 0;
-        switch (rotation) {
-            case Surface.ROTATION_0:
-                degrees = 0;
-                break;
-            case Surface.ROTATION_90:
-                degrees = 90;
-                break;
-            case Surface.ROTATION_180:
-                degrees = 180;
-                break;
-            case Surface.ROTATION_270:
-                degrees = 270;
-                break;
-        }
-        int result;
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-            result = (info.orientation + degrees) % 360;
-            result = (360 - result) % 360;  // compensate the mirror
-        } else {  // back-facing
-            result = (info.orientation - degrees + 360) % 360;
-        }
-        //核心设置方法: 内部传入旋转角度
-        camera.setDisplayOrientation(result);
-    }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
@@ -166,10 +148,32 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
         switch (v.getId()) {
             case R.id.main_press_control:
-                switch (action){
+                switch (action) {
                     case MotionEvent.ACTION_DOWN:
                         if (ex > left && ex < right) {
+                            // TODO: 2016/10/20 开始录制视频, 进度条开始走
                             mProgressBar.setVisibility(View.VISIBLE);
+                            //开始录制
+                            Toast.makeText(this, "开始录制", Toast.LENGTH_SHORT).show();
+                            startRecord();
+                            mProgress = 0;
+                            mProgressThread = new Thread(){
+                                @Override
+                                public void run() {
+                                    super.run();
+                                    try {
+                                        while (true){
+                                            mProgress++;
+                                            mHandler.obtainMessage(0).sendToTarget();
+                                            Thread.sleep(20);
+                                        }
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            };
+
+                            mProgressThread.start();
                             ret = true;
                         }
 
@@ -177,9 +181,11 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
                     case MotionEvent.ACTION_UP:
                         if (ex > left && ex < right) {
                             mProgressBar.setVisibility(View.INVISIBLE);
+                            //判断是否为录制结束, 或者为成功录制(时间过短)
+                            //停止录制
+                            stopRecord();
                             ret = false;
                         }
-
                         break;
                 }
                 break;
@@ -192,6 +198,86 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     ///////////////////////////////////////////////////////////////////////////
     @Override
     public void onProgressEndListener() {
-        //视频直接保存
+        //视频停止录制
     }
+
+    /**
+     * 开始录制
+     */
+    private void startRecord() {
+        if (mMediaRecorder != null) {
+            //没有外置存储, 直接停止录制
+            if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                return;
+            }
+            try {
+                //mMediaRecorder.reset();
+                mCamera.unlock();
+                mMediaRecorder.setCamera(mCamera);
+                //从相机采集视频
+                mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                // 从麦克采集音频信息
+                mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                // TODO: 2016/10/20  设置视频格式
+                mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+                mMediaRecorder.setVideoSize(videoWidth, videoHeight);
+                //每秒的帧数
+                mMediaRecorder.setVideoFrameRate(24);
+                //编码格式
+                mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+                mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+                // TODO: 2016/10/20 临时写个文件地址, 稍候该!!!
+                File targetDir = Environment.
+                        getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+                mTargetFile = new File(targetDir,
+                        SystemClock.currentThreadTimeMillis() + ".mp4");
+                mMediaRecorder.setOutputFile(mTargetFile.getAbsolutePath());
+
+                mMediaRecorder.setPreviewDisplay(mSurfaceHolder.getSurface());
+                mMediaRecorder.prepare();
+                //正式录制
+                mMediaRecorder.start();
+                isRecording = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * 停止录制
+     */
+    private void stopRecord() {
+        if (isRecording) {
+            mMediaRecorder.stop();
+            isRecording = false;
+            Toast.makeText(this, "视频已经放至" + mTargetFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    //
+
+    private static class MyHandler extends Handler {
+        private WeakReference<MainActivity> mReference;
+        private MainActivity mActivity;
+        public MyHandler(MainActivity activity) {
+            mReference = new WeakReference<MainActivity>(activity);
+            mActivity = mReference.get();
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    mActivity.mProgressBar.setProgress(mActivity.mProgress);
+                    break;
+            }
+
+        }
+    }
+
+
 }
