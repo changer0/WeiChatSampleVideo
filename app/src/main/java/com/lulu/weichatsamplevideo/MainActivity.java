71907832fa03497dd0cbeda1ef9d0a2e7a3d0740
张鹿鹿 <zhanglulu1007@foxmail.com>
2016-10-23 20:38:17 +0800

    实现双击放大功能, 发现线程bug(已解决)

--- app/src/main/java/com/lulu/weichatsamplevideo/MainActivity.java
+++ app/src/main/java/com/lulu/weichatsamplevideo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.lulu.weichatsamplevideo;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.content.pm.ActivityInfo;
 import android.hardware.Camera;
 import android.media.MediaRecorder;
@@ -10,6 +12,8 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -20,6 +24,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, View.OnTouchListener, BothWayProgressBar.OnProgressEndListener {
@@ -27,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     private static final int LISTENER_START = 200;
     private static final String TAG = "MainActivity";
     //预览SurfaceView
-    private SurfaceView mPreview;
+    private SurfaceView mSurfaceView;
     private Camera mCamera;
     //底部"按住拍"按钮
     private View mStartButton;
@@ -42,16 +47,22 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     private int videoWidth, videoHeight;
     //判断是否正在录制
     private boolean isRecording;
+    //段视频保存的目录
     private File mTargetFile;
     //当前进度/时间
     private int mProgress;
     //录制最大时间
     public static final int MAX_TIME = 10;
-    //
+    //是否上滑取消
     private boolean isCancel;
+    //手势处理, 主要用于变焦 (双击放大缩小)
+    private GestureDetector mDetector;
+    //是否放大
+    private boolean isZoomIn = false;
 
     private MyHandler mHandler;
     private TextView mTvTip;
+    private boolean isRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,9 +75,21 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     private void initView() {
         videoWidth = 640;
         videoHeight = 480;
-        mPreview = (SurfaceView) findViewById(R.id.main_surface_view);
+        mSurfaceView = (SurfaceView) findViewById(R.id.main_surface_view);
 
-        mSurfaceHolder = mPreview.getHolder();
+        mDetector = new GestureDetector(this, new ZoomGestureListener());
+        /**
+         * 单独处理mSurfaceView的双击事件
+         */
+        mSurfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mDetector.onTouchEvent(event);
+                return true;
+            }
+        });
+
+        mSurfaceHolder = mSurfaceView.getHolder();
         //设置屏幕分辨率
         mSurfaceHolder.setFixedSize(videoWidth, videoHeight);
         mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
@@ -79,7 +102,6 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         mProgressBar = (BothWayProgressBar) findViewById(R.
                 id.main_progress_bar);
         mProgressBar.setOnProgressEndListener(this);
-
         mHandler = new MyHandler(this);
         mMediaRecorder = new MediaRecorder();
     }
@@ -108,7 +130,10 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         Log.d(TAG, "startPreView: ");
 
         if (mCamera == null) {
-            mCamera = Camera.open();
+            mCamera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+        }
+        if (mMediaRecorder == null) {
+            mMediaRecorder = new MediaRecorder();
         }
         if (mCamera != null) {
             mCamera.setDisplayOrientation(90);
@@ -144,14 +169,151 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
             //停止预览并释放摄像头资源
             mCamera.stopPreview();
             mCamera.release();
-
+            mCamera = null;
         }
         if (mMediaRecorder != null) {
             mMediaRecorder.release();
+            mMediaRecorder = null;
         }
     }
 
 
+    ///////////////////////////////////////////////////////////////////////////
+    // 进度条结束后的回调方法
+    ///////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onProgressEndListener() {
+        //视频停止录制
+        stopRecordSave();
+    }
+
+    /**
+     * 开始录制
+     */
+    private void startRecord() {
+        if (mMediaRecorder != null) {
+            //没有外置存储, 直接停止录制
+            if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                return;
+            }
+            try {
+                //mMediaRecorder.reset();
+                mCamera.unlock();
+                mMediaRecorder.setCamera(mCamera);
+                //从相机采集视频
+                mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                // 从麦克采集音频信息
+                mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                // TODO: 2016/10/20  设置视频格式
+                mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+                mMediaRecorder.setVideoSize(videoWidth, videoHeight);
+                //每秒的帧数
+                mMediaRecorder.setVideoFrameRate(24);
+                //编码格式
+                mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+                mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                // 设置帧频率，然后就清晰了
+                mMediaRecorder.setVideoEncodingBitRate(1 * 1024 * 1024 * 100);
+                // TODO: 2016/10/20 临时写个文件地址, 稍候该!!!
+                File targetDir = Environment.
+                        getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+                mTargetFile = new File(targetDir,
+                        SystemClock.currentThreadTimeMillis() + ".mp4");
+                mMediaRecorder.setOutputFile(mTargetFile.getAbsolutePath());
+
+                mMediaRecorder.setPreviewDisplay(mSurfaceHolder.getSurface());
+                mMediaRecorder.prepare();
+                //正式录制
+                mMediaRecorder.start();
+                isRecording = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * 停止录制 并且保存
+     */
+    private void stopRecordSave() {
+        if (isRecording) {
+            isRunning = false;
+            mMediaRecorder.stop();
+            isRecording = false;
+            Toast.makeText(this, "视频已经放至" + mTargetFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 停止录制, 不保存
+     */
+    private void stopRecordUnSave() {
+        if (isRecording) {
+            isRunning = false;
+            mMediaRecorder.stop();
+            isRecording = false;
+            if (mTargetFile.exists()) {
+                //不保存直接删掉
+                mTargetFile.delete();
+            }
+        }
+    }
+
+    /**
+     * 相机变焦
+     *
+     * @param zoomValue
+     */
+    public void setZoom(int zoomValue) {
+        if (mCamera != null) {
+            Camera.Parameters parameters = mCamera.getParameters();
+            if (parameters.isZoomSupported()) {//判断是否支持
+                int maxZoom = parameters.getMaxZoom();
+                if (maxZoom == 0) {
+                    return;
+                }
+                if (zoomValue > maxZoom) {
+                    zoomValue = maxZoom;
+                }
+                parameters.setZoom(zoomValue);
+                mCamera.setParameters(parameters);
+            }
+        }
+
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Handler处理
+    ///////////////////////////////////////////////////////////////////////////
+    private static class MyHandler extends Handler {
+        private WeakReference<MainActivity> mReference;
+        private MainActivity mActivity;
+
+        public MyHandler(MainActivity activity) {
+            mReference = new WeakReference<MainActivity>(activity);
+            mActivity = mReference.get();
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    mActivity.mProgressBar.setProgress(mActivity.mProgress);
+                    break;
+            }
+
+        }
+    }
+
+    /**
+     * 触摸事件的触发
+     *
+     * @param v
+     * @param event
+     * @return
+     */
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         boolean ret = false;
@@ -166,7 +328,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         float downY = 0;
 
         switch (v.getId()) {
-            case R.id.main_press_control:
+            case R.id.main_press_control: {
                 switch (action) {
                     case MotionEvent.ACTION_DOWN:
                         if (ex > left && ex < right) {
@@ -188,7 +350,8 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
                                     super.run();
                                     try {
                                         mProgress = 0;
-                                        while (true) {
+                                        isRunning = true;
+                                        while (isRunning) {
                                             mProgress++;
                                             mHandler.obtainMessage(0).sendToTarget();
                                             Thread.sleep(20);
@@ -202,6 +365,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
                             mProgressThread.start();
                             ret = true;
                         }
+
 
                         break;
                     case MotionEvent.ACTION_UP:
@@ -240,106 +404,36 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
                         break;
                 }
                 break;
+
+            }
+
         }
         return ret;
     }
 
     ///////////////////////////////////////////////////////////////////////////
-    // 进度条结束后的回调方法
+    // 变焦手势处理类
     ///////////////////////////////////////////////////////////////////////////
-    @Override
-    public void onProgressEndListener() {
-        //视频停止录制
-    }
-
-    /**
-     * 开始录制
-     */
-    private void startRecord() {
-        if (mMediaRecorder != null) {
-            //没有外置存储, 直接停止录制
-            if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                return;
-            }
-            try {
-                //mMediaRecorder.reset();
-                mCamera.unlock();
-                mMediaRecorder.setCamera(mCamera);
-                //从相机采集视频
-                mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-                // 从麦克采集音频信息
-                mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                // TODO: 2016/10/20  设置视频格式
-                mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-                mMediaRecorder.setVideoSize(videoWidth, videoHeight);
-                //每秒的帧数
-                mMediaRecorder.setVideoFrameRate(24);
-                //编码格式
-                mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
-                mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-                // TODO: 2016/10/20 临时写个文件地址, 稍候该!!!
-                File targetDir = Environment.
-                        getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
-                mTargetFile = new File(targetDir,
-                        SystemClock.currentThreadTimeMillis() + ".mp4");
-                mMediaRecorder.setOutputFile(mTargetFile.getAbsolutePath());
-
-                mMediaRecorder.setPreviewDisplay(mSurfaceHolder.getSurface());
-                mMediaRecorder.prepare();
-                //正式录制
-                mMediaRecorder.start();
-                isRecording = true;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
-
-    /**
-     * 停止录制
-     */
-    private void stopRecordSave() {
-        if (isRecording) {
-            mMediaRecorder.stop();
-            isRecording = false;
-            Toast.makeText(this, "视频已经放至" + mTargetFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void stopRecordUnSave() {
-        if (isRecording) {
-            mMediaRecorder.stop();
-            isRecording = false;
-            if (mTargetFile.exists()) {
-                //不保存直接删掉
-                mTargetFile.delete();
-            }
-        }
-    }
 
 
-    //
-
-    private static class MyHandler extends Handler {
-        private WeakReference<MainActivity> mReference;
-        private MainActivity mActivity;
-
-        public MyHandler(MainActivity activity) {
-            mReference = new WeakReference<MainActivity>(activity);
-            mActivity = mReference.get();
-        }
-
+    class ZoomGestureListener extends GestureDetector.SimpleOnGestureListener {
+        //双击手势事件
         @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case 0:
-                    mActivity.mProgressBar.setProgress(mActivity.mProgress);
-                    break;
+        public boolean onDoubleTap(MotionEvent e) {
+            super.onDoubleTap(e);
+            Log.d(TAG, "onDoubleTap: 双击事件");
+            if (mMediaRecorder != null) {
+                if (!isZoomIn) {
+                    setZoom(20);
+                    isZoomIn = true;
+                } else {
+                    setZoom(0);
+                    isZoomIn = false;
+                }
             }
-
+            return true;
         }
+
     }
 
 
